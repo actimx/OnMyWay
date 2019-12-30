efbedf662bbf5e0b6935474e29196c93bb6c2f67
Jonatanflova <46691965+Jonatanflova@users.noreply.github.com>
2019-12-30 17:11:56 -0600

    [add] Expo Google App Auth
    Instalación de expo google app auth

--- On-my-way-Project/src/views/Login/index.js
+++ On-my-way-Project/src/views/Login/index.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { Image } from 'react-native';
+import * as Google from 'expo-google-app-auth';
 import { Container, Footer, FooterTab, Content, Grid, Form, Input, Label, Item, Icon, Button, Text, Body  } from 'native-base';
 
 import style from './style';
-
-import { SIGNIN, HOME } from '../../consts';
+import enviroment from "../../../enviroment";
+import { saveItem } from "../../utils/storage";
+import { ACCESS_TOKEN, USER_INFO, GOOGLE_SUCCESS_MESSAGE, HOME, SIGNIN } from "../../consts";
 
 const GOOGLE_IMAGE = require('../../../assets/google-icon.png');
 const FACEBOOK_IMAGE = require('../../../assets/facebook-icon.png');
 const LOGO_OFICIAL = require('../../../assets/onmyway-logo.png');
 // const SUCCES_ICON = require('../../../assets/log-in-icon.png');
+const {
+    iosClientId,
+    androidClientId,
+    iosStandaloneAppClientId,
+    androidStandaloneAppClientId
+} = enviroment();
 
 export default function Login({navigation}) {
+
+    const handleLoginPress = async () => {
+        try {
+            const { user, accessToken, type } = await Google.logInAsync({
+                iosClientId,
+                androidClientId,
+                iosStandaloneAppClientId,
+                androidStandaloneAppClientId
+            });
+    
+            if (type === GOOGLE_SUCCESS_MESSAGE) {
+                const userResult = await saveItem(USER_INFO, JSON.stringify(user));
+                const tokenResult = await saveItem(ACCESS_TOKEN, accessToken);
+
+                if (userResult && tokenResult) {
+                    navigation.navigate(HOME);
+                } else {
+                    alert('Error al iniciar sesión');
+                }
+            } 
+        } catch (error) {
+            alert('Error: ' + error);
+        }
+    };
     return (
         <Container>
             <Content contentContainerStyle={style.content}>
@@ -38,9 +70,7 @@ export default function Login({navigation}) {
                     </Item>
 
                     <Button style={style.googleBtn} light
-                        onPress={() => {
-                            navigation.navigate(HOME);
-                        }}
+                        onPress={handleLoginPress}
                         >
                         <Image source={GOOGLE_IMAGE} style={style.googleIcon}/>
                     </Button>
